2ca398154690bc4bb0e1b91dd676091ef216a9a2
ElKamelHK <hamdiek58@gmail.com>
2021-08-05 12:56:33 +0100

    initialisation

---
+++ src/main/java/com/sysgestionemployer/sge/SystemeDeGestionDesEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.sysgestionemployer.sge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeDeGestionDesEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeDeGestionDesEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysgestionemployer/sge/SystemeDeGestionDesEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysgestionemployer.sge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeDeGestionDesEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
